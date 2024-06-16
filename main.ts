0da4b1c28975665c2d8b52f3769e7fd805ede7c8
AbdulRehman1770 <rehman71gtx@gmail.com>
2024-06-16 18:04:23 +0100

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+async function Word_Counter(){
+    console.log(chalk.bold.cyanBright("\n \t\tWords Counter Code\n"));
+
+
+    let Word = await inquirer.prompt({
+        message : "Enter A Sentence",
+        type : "input",
+        name : "Sentence"
+    })
+
+    let WordsCounter = Word.Sentence.trim().split(" ");
+    console.log(`Total Words : ${WordsCounter.length}`);}
+
+Word_Counter()
